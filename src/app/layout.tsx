97ee3520c2ba7fbd6e96f7b26534dfe10ccd0833
Saw Simon Linn <simonlinn2@gmail.com>
2025-08-19 06:47:24 -0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
